b7bd5542464277564a50e6ebf1d2933d87bb7858
Viitanen Akseli Jalmari <akseli.j.viitanen@student.jyu.fi>
2023-10-24 15:00:49 +0300

    Initial commit

---
+++ BillManager/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BillManager;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ BillManager/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace BillManager;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
