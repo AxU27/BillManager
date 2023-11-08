f35d11ceeef02aa4b83e998370bf7945a58dbb99
AxU27 <139479676+AxU27@users.noreply.github.com>
2023-11-08 18:09:32 +0200

    First things

---
+++ BillManager/Model/Bill.cs
@@ -0,0 +1,9 @@
+﻿namespace BillManager.Model;
+
+public class Bill
+{
+    public string name { get; set; }
+    public string receiver { get; set; }
+    public float price { get; set; }
+    public DateOnly dueDate { get; set; }
+}
