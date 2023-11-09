aa7d3299918fc2c117df8035e90731b81e84321b
AxU27 <139479676+AxU27@users.noreply.github.com>
2023-11-09 08:25:43 +0200

    Create BillViewModel.cs

---
+++ BillManager/ViewModel/BillViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BillManager.Model;
+
+namespace BillManager.ViewModel
+{
+    
+    public partial class BillViewModel : BaseViewModel
+    {
+        public ObservableCollection<Bill> Bills { get; } = new();
+
+        public BillViewModel()
+        {
+            Title = "asd";
+        }
+    }
+}
